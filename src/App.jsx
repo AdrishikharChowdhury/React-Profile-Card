19f9efca44539226668fddc791028f4c80f454b8
AdrishikharChowdhury <amiadishikhar@gmail.com>
2025-08-26 07:54:46 +0530

    updated bio

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
 
   const username="Adrishikhar Chowdhury";
   const roles="Frontend Developer";
-  const bio="Hi, I am passionate about creating things and have fun.";
+  const bio="Hi, I am passionate about creating things and having fun.";
   const location="Kolkata, IN";
   const skills="C,C++, Python, Java, HTML, CSS, JS, React, Tailwind"
   return (
